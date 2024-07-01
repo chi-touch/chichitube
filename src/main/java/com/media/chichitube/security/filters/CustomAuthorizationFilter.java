e0bda67f4fbddf5a6ff94820adac561bc573e56a
chi-touch <gloebudav@gmail.com>
2024-07-01 12:17:15 +0100

    "spring boot test to security "

---
+++ src/main/java/com/media/chichitube/security/filters/CustomAuthorizationFilter.java
@@ -0,0 +1,9 @@
+package com.media.chichitube.security.filters;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthorizationFilter {
+
+}
--- src/main/java/com/media/chichitube/security/filters/CustomerAuthorizationFilter.java
+++
@@ -1,5 +0,0 @@
-package com.media.chichitube.security.filters;
-
-public class CustomerAuthorizationFilter {
-
-}
